0a188e95a4e6db4bb4ed85bd5b573ab80b5c6aea
dmallia17 <Daniel.Mallia21@myhunter.cuny.edu>
2020-01-07 13:42:07 -0500

    Add decrement functionality

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// Decrement component
+class Decrement extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentNumber : props.myNumber
+    }
+    //this.decrementNumber = this.decrementNumber.bind(this); - ALTERNATE METHOD
+  }
+
+  decrementNumber = () => {
+    if(this.state.currentNumber <= 0) {
+      alert("Cannot be less than zero.");
+    } else {
+      this.setState({currentNumber: this.state.currentNumber-1});
+    }
+  }
+
+  render() {
+    return <div>
+      <h1>Current Number: {this.state.currentNumber} </h1>
+      <button onClick={this.decrementNumber}>Decrement</button>
+      </div>
+  }
+}
+
+// Define Decrement proptypes
+Decrement.propTypes = {
+  myNumber : PropTypes.number.isRequired,
+}
+
+class App extends Component {
+  render() {
+    return <div>
+      <Decrement myNumber="10"></Decrement>
+    </div>;
+  }
 }
 
 export default App;
